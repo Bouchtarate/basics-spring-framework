9aa47893b66776e858321e741962b87dc0a60314
hamza bouchtarate <hamza.bouchtarate60@gmail.com>
2025-03-17 23:11:57 +0000

    Start learn spring framework

---
+++ src/main/java/com/example/Dev.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Dev {
+
+    public Dev() {
+        System.out.println("Dev constructor");
+    }
+
+    public void build() {
+
+        System.out.println(
+                "Build a project by using spring framework");
+    }
+}
